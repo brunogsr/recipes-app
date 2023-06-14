2dd51edc9cb5b3639e92d51d5866a9747efdf6da
lucastelesss <lucastelessouza005@gmail.com>
2023-06-14 11:31:30 -0300

    requisito 38 - tela receita em progresso - adiciona checkbox em cada ingrediente listado

--- src/pages/RecipeInProgress.js
+++ src/pages/RecipeInProgress.js
@@ -9,6 +9,7 @@ function RecipeInProgress() {
   const endpoint = location.includes('/meals') ? 'https://www.themealdb.com/api/json/v1/1/lookup.php?i=' : 'https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=';
 
   const [recipeById, setRecipeById] = useState(null);
+  const [ingredients, setIngredients] = useState([]);
 
   useEffect(() => {
     const fetchIdRecipe = async () => {
@@ -19,6 +20,33 @@ function RecipeInProgress() {
     };
     fetchIdRecipe();
   }, []);
+
+  useEffect(() => {
+    console.log(recipeById);
+
+    const numberOfIngredients = 20;
+    const temporaryIngredients = [];
+
+    if (recipeById !== null) {
+      for (let numberIngredient = 1; numberIngredient <= numberOfIngredients;
+        numberIngredient += 1) {
+        const ingredientNumber = `strIngredient${numberIngredient}`;
+        const ingredientValue = recipeById[ingredientNumber];
+
+        if (ingredientValue !== null
+          && ingredientValue !== ''
+          && ingredientValue !== undefined) {
+          temporaryIngredients.push(ingredientValue);
+        }
+      }
+    }
+
+    setIngredients(temporaryIngredients);
+  }, [recipeById]);
+
+  useEffect(() => {
+    console.log(ingredients);
+  }, [ingredients]);
 
   return (
     <div>
@@ -49,6 +77,17 @@ function RecipeInProgress() {
             { recipeById.strAlcoholic
           || recipeById.strCategory }
           </p>
+          <h3>Ingredients</h3>
+          { ingredients && ingredients.map((ingredient, index) => (
+            <label
+              key={ index }
+              data-testid={ `${index}-ingredient-step` }
+            >
+              {ingredient}
+              <input type="checkbox" />
+            </label>
+          )) }
+          <h3>Instructions</h3>
           <p data-testid="instructions">{ recipeById.strInstructions }</p>
           <button data-testid="finish-recipe-btn">Finish Recipe</button>
         </main>
